9b0d17bc5dc3c2cae0cb571dbb194069bd4a2b26
Amanda Droghini <adroghini@alaska.edu>
2022-12-28 13:26:57 -0900

    Changed file path

--- package_Statistics/init.R
+++ package_Statistics/init.R
@@ -1,6 +1,6 @@
 # Define directories
-drive <- "D:"
-root_folder <- "ACCS_Work/GMU_17_Moose"
+drive <- "C:"
+root_folder <- "ACCS_Work/Projects/Moose_SouthwestAlaska"
 input_dir <- file.path(drive, root_folder, "Data_01_Input")
 pipeline_dir <- file.path(drive, root_folder, "Data_02_Pipeline")
 output_dir <- file.path(drive, root_folder, "Data_03_Output","pathSelectionFunction")
@@ -11,6 +11,7 @@ library(tidyverse)
 # Statistics packages
 library(survival)
 library(infer)
+library(glmmTMB)
 
 # Functions
 source(paste0(git_dir,"function-iterateModelRun.R"))
